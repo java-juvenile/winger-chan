4c1a9058e910e2cf4740e5234369da98c8d1242a
Winger Chan <winger.chan@outlook.com>
2020-01-14 01:45:35 +0800

    Optimize performance
    Signed-off-by: Winger Chan <winger.chan@outlook.com>

--- src/main/java/com/maxwit/course/MyDarkClient.java
+++ src/main/java/com/maxwit/course/MyDarkClient.java
@@ -4,28 +4,62 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MyDarkClient {
-    public static void main(String[] args) throws UnknownHostException, IOException {
+    public static File findFile(String file) {
+        // getting the working directory of the current program in Java
+        String currentPath = System.getProperty("user.dir");
+
+        String pattern = "(?!.*/).+";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(file);
+        String path = null;
+        if (m.find()) {
+            path = "/" + m.group(0);
+        }
+        // use class File to fing a file
+        File f = new File(currentPath + path);
+
+        return f;
+    }
+
+    public static void writeFile(StringBuilder ss, File f) throws IOException {
+        String str = ss.toString();
+
+        String[] strList = str.split("\n");
+        str = strList[2];
+        String[] firstLine = strList[0].split(" ");
+        
+        if (Integer.parseInt(firstLine[1]) == 200) {
+            OutputStream out = null;        
+            out = new FileOutputStream(f);
+            byte[] b = str.getBytes("UTF-8");
+            // output content and saves file
+            out.write(b);
+            out.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] list = args[0].split(" ");
+        String file = list[1];
+        String reqType = "GET";
+        String reqHttp = "HTTP/1.1";
+        String reqLine = reqType + " " + file + " " + reqHttp;
+        
         String[] url = args[1].split(":");
         String host = url[0];
         int port = Integer.parseInt(url[1]);
 
         Socket socket = new Socket(host, port);
         OutputStream outputStream = socket.getOutputStream();
-
-        String[] list = args[0].split(" ");
-        String file = list[1];
-
-        socket.getOutputStream().write(file.getBytes("UTF-8"));
+        socket.getOutputStream().write(reqLine.getBytes("UTF-8"));
         socket.shutdownOutput();
 
         InputStream inputStream = socket.getInputStream();
         byte[] bytes = new byte[1024];
-
         int len;
         StringBuilder ss = new StringBuilder();
 
@@ -33,26 +67,10 @@ public class MyDarkClient {
             ss.append(new String(bytes, 0, len, "UTF-8"));
         }
 
-        // getting the working directory of the current program in Java
-        String currentPath = System.getProperty("user.dir");
+        File f = findFile(file);
+        writeFile(ss, f);
 
-        String patternPath = "(?!.*/).+";
-        Pattern rPath = Pattern.compile(patternPath);
-        Matcher mPath = rPath.matcher(file);
-        String path = null;
-        if (mPath.find()) {
-            path = "/" + mPath.group(0);
-        }
-        // use class File to fing a file
-        File f = new File(currentPath + path);
-
-        OutputStream out = null;
-        // instantiation
-        out = new FileOutputStream(f);
-        // output content and saves file
-        out.write(bytes);
-
-        out.close();
+        inputStream.close();
         outputStream.close();
         socket.close();
     }
--- src/main/java/com/maxwit/course/MyPagServer.java
+++ src/main/java/com/maxwit/course/MyPagServer.java
@@ -6,12 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MyPagServer {
-    public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(Integer.parseInt(args[0]));
-
-        Socket socket = server.accept();
-
-        InputStream inputStream = socket.getInputStream();
+    public static StringBuilder inputStream(Socket socket, InputStream inputStream) throws IOException {
         byte[] bytes = new byte[1024];
 
         int len;
@@ -20,14 +15,49 @@ public class MyPagServer {
             ss.append(new String(bytes, 0, len, "UTF-8"));
         }
 
-        // getting the working directory of the current program in Java
-        String currentPath = System.getProperty("user.dir");
+        return ss;
+    }
 
+    public static void outputStream(Socket socket, OutputStream outputStream, String repHttp, int repNum, String repStatu, String message, File f)
+            throws IOException {
+
+        if (!f.exists()) {
+            repNum = 404;
+            repStatu = "Not Found";
+        }
+        String news = repHttp + " " + repNum + " " + repStatu + "\n\n" + message;
+        outputStream.write(news.getBytes("UTF-8"));
+    }
+
+    public static File fileContent(StringBuilder ss) {
+
+        String line = ss.toString();
+        String[] list = line.split(" ");
+
+        String file = list[1];
+        String currentPath = System.getProperty("user.dir");
         // use class File to fing a file
-        File f = new File(currentPath + ss);
-        String message = String.valueOf(f.length());
+        File f = new File(currentPath + file);
+        
+        return f;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String repHttp = "HTTP/1.1";
+        int repNum = 200;
+        String repStatu = "OK";
+
+        ServerSocket server = new ServerSocket(Integer.parseInt(args[0]));
+        Socket socket = server.accept();
+        InputStream inputStream = socket.getInputStream();
         OutputStream outputStream = socket.getOutputStream();
-        outputStream.write(message.getBytes("UTF-8"));
+        
+        StringBuilder ss = inputStream(socket, inputStream);
+        // getting the working directory of the current program in Java      
+
+        File file = fileContent(ss);
+        String message = String.valueOf(file.length());
+        outputStream(socket, outputStream, repHttp, repNum, repStatu, message, file);
 
         inputStream.close();
         outputStream.close();
